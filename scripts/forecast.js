34e266b7efbe9a59c8ba592b5e40e2978258a739
Mégane <megane.londono@hotmail.com>
2021-11-21 16:06:45 +0100

    weather app with local storage

---
+++ scripts/forecast.js
@@ -0,0 +1,23 @@
+const key = "9SY6DDqliodQ3zUl0aSD4yAwAIvYxYqS";
+
+// get weather information
+const getWeather = async (id) => {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// get city information
+const getCity = async (city) => {
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI =  (data) => {
+    console.log(data)
+    /* const cityDetails = data.cityDetails;
+    const weather = data.weather; */
+
+    // destructuring properties - same functionality as above
+    const { cityDetails, weather } = data;
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+        </div>
+    `;
+
+    // update the night/day icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute("src", iconSrc);
+
+    let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+    time.setAttribute("src", timeSrc);
+
+    // remove the d-none class if present
+    if (card.classList.contains("d-none")){
+        card.classList.remove("d-none");
+    }
+}
+
+const updateCity = async (city) => {
+    const cityDetails = await getCity(city);
+    const weather = await getWeather(cityDetails.Key);
+
+    // object shorthand notation
+    return { cityDetails: cityDetails, weather: weather };
+}
+
+cityForm.addEventListener("submit", e => {
+    e.preventDefault();
+
+    // get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    // update the UI with the new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+
+    // set local storage
+    localStorage.setItem("city", city);
+})
+
+if (localStorage.getItem("city")) {
+    updateCity(localStorage.getItem("city"))
+        .then(data => updateUI(data))
+        .catch(err => console.log(err))
+}
